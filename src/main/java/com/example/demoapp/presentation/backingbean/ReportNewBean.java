ddd9e845bd6e5011dea3e598ea7da07600e15f43
aion0721 <rudolf.aion@gmail.com>
2025-01-11 19:32:47 +0900

    Apply primaface

--- src/main/java/com/example/demoapp/presentation/backingbean/ReportNewBean.java
+++ src/main/java/com/example/demoapp/presentation/backingbean/ReportNewBean.java
@@ -1,11 +1,13 @@
 package com.example.demoapp.presentation.backingbean;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.inject.Named;
 
 @Named
 @RequestScoped
 public class ReportNewBean{
+    @NotBlank(message = "タイトルは空白にできません")
 	private String title;
 	private String detail;
 
