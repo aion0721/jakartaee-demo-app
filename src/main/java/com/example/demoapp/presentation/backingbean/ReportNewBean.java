25b7411bb0b79bf4b9ea933629baed663e9f9f9b
aion0721 <rudolf.aion@gmail.com>
2025-01-11 19:34:51 +0900

    add lombok

--- src/main/java/com/example/demoapp/presentation/backingbean/ReportNewBean.java
+++ src/main/java/com/example/demoapp/presentation/backingbean/ReportNewBean.java
@@ -4,26 +4,16 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.inject.Named;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Named
 @RequestScoped
+@Getter
+@Setter
 public class ReportNewBean{
     @NotBlank(message = "タイトルは空白にできません")
 	private String title;
 	private String detail;
 
-	public String getTitle(){
-		return title;
-	}
-
-	public void setTitle(String title){
-		this.title = title;
-	}
-
-	public String getDetail(){
-		return detail;
-	}
-
-	public void setDetail(String detail){
-		this.detail = detail;
-	}
 }
